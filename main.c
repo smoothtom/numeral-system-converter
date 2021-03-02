564e0ecc8bd46e5a7f5a298b5d5a43018a975eda
smoothtom <t-lauth@web.de>
2021-03-02 20:58:10 +0100

    Hexadecimal to Octal function is done

--- main.c
+++ main.c
@@ -163,11 +163,33 @@ int HexadecimaltoDecimal() {
 }
 
 int HexadecimaltoOctal() {
-  char hex[32] = {0};
-
+  char hex[32] = {0}, c;
+  int n, i, len, dec = 0, oct = 0;
   printf("Please Enter a Hexadecimal number: ");
-  scanf("%d", &hex);
+  scanf("%s", hex);
 
+  for (len = 0; hex[len] != '\0'; len++);
+  for (i = 0; hex[i] != '\0'; i++, len--) {
+    if (hex[i] >= '0' && hex[i] <= '9') {
+      dec = dec + (hex[i] - '0') * pow (16, len-1);
+    }
+    if (hex[i] >= 'A' && hex[i] <= 'F') {
+      dec = dec + (hex[i] - 55) * pow (16, len-1);
+    }
+    if (hex[i] >= 'a' && hex[i] <= 'f') {
+      dec = dec + (hex[i] - 87) * pow (16, len-1);
+    }
+  }
+
+  i = 1; 
+  while (dec != 0) {
+    oct = oct + (dec % 8) * i;
+    dec = dec / 8;
+    i = i * 10;
+  }
+
+  printf("%s in Hexadecimal = %d in Octal\n", hex,oct);
+  secondmenu();
   return 0;
 }
 
